2a00f79f17b09a364ea0d0f682a61faf6fcf7e06
Andrew Moss <agordonmoss@gmail.com>
2019-09-04 20:18:10 -0600

    Start on device profiles

--- index.js
+++ index.js
@@ -11,19 +11,25 @@ var config = require("./config");
 var tank_iot_01 = new TankDevice(
   dsn = "tank_iot_01",
   type = "tank",
-  connectionString = config.connection_strings.device.tank_iot_01);
+  connectionString = config.connection_strings.device.tank_iot_01,
+  profile = null,
+  print = false);
 
 // Wind Monitoring Device
 var wind_iot_01 = new WindDevice(
   dsn = "wind_iot_01",
   type = "wind",
-  connectionString = config.connection_strings.device.wind_iot_01);
+  connectionString = config.connection_strings.device.wind_iot_01,
+  profile = "profile1",
+  print = false);
 
 // Energy Monitoring Device
 var energy_iot_01 = new EnergyDevice(
   dsn = "energy_iot_01",
   type = "energy",
-  connectionString = config.connection_strings.device.energy_iot_01);
+  connectionString = config.connection_strings.device.energy_iot_01,
+  profile = "profile1",
+  print = false);
 
 //Facility 2
 
@@ -31,26 +37,32 @@ var energy_iot_01 = new EnergyDevice(
 var tank_iot_02 = new TankDevice(
     dsn = "tank_iot_02",
     type = "tank",
-    connectionString = config.connection_strings.device.tank_iot_02);
+    connectionString = config.connection_strings.device.tank_iot_02,
+    profile = null,
+    print = false)
   
 // Wind Monitoring Device
 var wind_iot_02 = new WindDevice(
     dsn = "wind_iot_02",
     type = "wind",
-    connectionString = config.connection_strings.device.wind_iot_02);
+    connectionString = config.connection_strings.device.wind_iot_02,
+    profile = "profile2",
+    print = false)
 
 // Energy Monitoring Device
 var energy_iot_02 = new EnergyDevice(
     dsn = "energy_iot_02",
     type = "energy",
-    connectionString = config.connection_strings.device.energy_iot_02);
+    connectionString = config.connection_strings.device.energy_iot_02,
+    profile = "profile2",
+    print = false);
 
 
-var facility_01 = new Facility(name = "facility_01", devices =  [energy_iot_01,wind_iot_01,tank_iot_01],geolocation = {"lat":51.0447,"lon":114.0719});
+var facility_01 = new Facility(name = "facility_01", devices =  [energy_iot_01,wind_iot_01,tank_iot_01],geolocation = {"lat":51.0447,"lon":114.0719},frequency = 60000);
 facility_01.setupFacility();
 facility_01.putOnline();
 
 
-var facility_02 = new Facility(name = "facility_02", devices =  [energy_iot_02,wind_iot_02,tank_iot_02],geolocation = {"lat":53.5461,"lon":113.4938});
+var facility_02 = new Facility(name = "facility_02", devices =  [energy_iot_02,wind_iot_02,tank_iot_02],geolocation = {"lat":53.5461,"lon":113.4938},frequency = 60000);
 facility_02.setupFacility();
 facility_02.putOnline();
